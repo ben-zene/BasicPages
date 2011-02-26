ce6bc3f5f32dc6b935caa052ca8cdc8a0ac2b3ae
Ben Merrill <ben@inceptioneng.com>
2011-02-25 18:51:16 -0700

    Removing default bp_config.php so installer loads first run.

--- bp_config.php
+++
@@ -1,7 +0,0 @@
-<?php defined("BP_START") || die("Unauthorized Access");
-/* This file was automatically generated on 2011-02-25T18:47:41-07:00 by a fresh install. */
-
-define("PATH", dirname(__FILE__));
-
-$bp_config = array (
-);
