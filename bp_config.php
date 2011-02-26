fdd678e69db5f8330bafaeb440ef742c50e96734
Ben Merrill <ben@inceptioneng.com>
2011-02-25 18:45:05 -0700

    Removing sample tags from config

--- bp_config.php
+++ bp_config.php
@@ -3,11 +3,14 @@
 define("PATH", dirname(__FILE__));
 
 $bp_config = array (
-  'admin_user' => 'ss_admin_user',
-  'admin_pass' => 'ss_admin_pass',
-  'admin_token' => 'ss_random_64',
-  'admin_email' => 'you@yourdomain.com',
-  'url' => 'http://ss_full_url',
+  'admin_user' => '',
+  'admin_pass' => '',
+  'admin_token' => '',
+  'admin_email' => '',
+  'url' => '',
   'default_theme' => 'basic',
   'default_style' => 'blue-grey.css',
+  'navigation' => 
+  array (
+  ),
 );
